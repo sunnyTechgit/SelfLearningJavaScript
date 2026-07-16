2159169b5c26013711c7d3a76c2306fb83f7ce77
sunnyTechgit <sunnyk86985@gmail.com>
2026-07-16 16:57:29 +0530

    Comparison of different data Types

---
+++ Class3/Comparison.js
@@ -0,0 +1,18 @@
+ console.log(2>1); // true
+console.log(2<1);   // false
+console.log(2>=1); // true
+console.log(2<=1);  // false
+console.log(2==1); // false
+console.log(2!=1);  // true
+console.log(2==="2");   // false
+console.log(2!=="2");  // true  
+console.log(2=="2"); // true
+console.log("2" ==undefined); // false
+console.log("2" ==null); // false
+      
+console.log(null==undefined); // true
+console.log(null===undefined); // false
+console.log(null==0); // false
+console.log(null>=0); // true
+console.log(null>0); // false
+console.log(null<=0); // true
