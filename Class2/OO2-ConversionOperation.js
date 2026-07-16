9964ee466bd4f57dbbcd431aabc0af084e8086cb
sunnyTechgit <sunnyk86985@gmail.com>
2026-07-16 16:39:12 +0530

    Conversion of data types

--- Class2/OO2-ConversionOperation.js
+++ Class2/OO2-ConversionOperation.js
@@ -1,12 +1,12 @@
-let score =" 85";
-console.log(typeof score); // string
-let numericScore = Number(score);
-console.log(typeof numericScore); // number
-console.log(numericScore); // 85
+// let score =" 85";
+// console.log(typeof score); // string
+// let numericScore = Number(score);
+// console.log(typeof numericScore); // number
+// console.log(numericScore); // 85
 
-let numString = "123abcd";
-let numericValue = Number(numString);
-console.log(numericValue);
+// let numString = "123abcd";
+// let numericValue = Number(numString);
+// console.log(numericValue);
  
 
 // Conversion from string , null, undefined, boolean to number 
@@ -20,15 +20,19 @@ console.log(numericValue);
 // 22 => "22"
 // 0 => "0"
 // 1 => "1"
-/* let num = 22; // true
-let num = -22; // true
-let num =0; false
-let isBool = Boolean(num);
-console.log(isBool); // true or false // "23" 
-let str = ""; // false
-let str = " "; // true
-let str = "23"; // true
-console.log(Boolean(str));
-*/
-let nu = null;
-console.log(typeof String(nu)); // "string"
+
+
+//  let num = 22; // true
+// let num = -22; // true
+// let num =0; false
+// let isBool = Boolean(num);
+// console.log(isBool); // true or false // "23" 
+// let str = ""; // false
+// let str = " "; // true
+// let str = "23"; // true
+// console.log(Boolean(str));
+
+// let nu = null;
+// console.log(typeof String(nu)); // "string"
+
+
